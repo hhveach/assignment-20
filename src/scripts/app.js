40e594076fc0e992a09d40d490d3c551201673e6
Henry Veach III <hhveach@Henrys-MacBook-Pro.local>
2017-02-17 13:36:53 -0500

    second commit

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,6 @@
 import Backbone from 'backbone';
 import $ from 'jquery';
-import {homeIntroducing, playlist} from './templates.js';
+import {homeIntroducing, playlist, playlistGreatest, playlistRoute} from './templates.js';
 import {bbcRadioCollection, bbcRadioModel} from './models.js';
 
 let insert = document.querySelector('.main-content');
@@ -11,10 +11,53 @@ const AppRouter = Backbone.Router.extend({
   },
 
   routes: {
-    '' : 'homePage',
+    'radio/1/:tod' : 'radioOneTod',
+    'radio/2/:tod' : 'radioTwoTod',
     'radio/1' : 'radioOne',
     'radio/2' : 'radioTwo',
+    'radio/3' : 'radioThree',
+    '' : 'homePage',
     '*default' : 'errorPage'
+  },
+
+  radioOneTod: function(tod){
+    let radioOneCollection = new bbcRadioCollection('1');
+      radioOneCollection.fetch().then(function(serverRes){
+        let results = '';
+        if(tod === 'morning'){
+          results = serverRes.playlist.a;
+        }
+
+        if(tod === 'afternoon'){
+          results = serverRes.playlist.b;
+        }
+
+        if(tod === 'evening'){
+          results = serverRes.playlist.c;
+        }
+        insert.innerHTML = playlistRoute(results, 1);
+      })
+  },
+
+  radioTwoTod: function(tod){
+    let radioOneCollection = new bbcRadioCollection('2');
+      radioOneCollection.fetch().then(function(serverRes){
+        let results = '';
+        if(tod === 'morning'){
+          results = serverRes.playlist.a;
+        }
+
+        if(tod === 'afternoon'){
+          results = serverRes.playlist.b;
+        }
+
+        if(tod === 'evening'){
+          results = serverRes.playlist.c;
+        }
+        insert.innerHTML = playlistRoute(results, 2);
+      })
+
+
   },
 
   homePage: function(){
@@ -29,19 +72,31 @@ const AppRouter = Backbone.Router.extend({
     let radioOneCollection = new bbcRadioCollection('1');
       radioOneCollection.fetch().then(function(serverRes){
         let resultsOne = serverRes.playlist.a;
-        // let resultsTwo = serverRes.playlist.b;
-        // let resultsThree = serverRes.playlist.c;
-          insert.innerHTML = playlist(resultsOne);
-          // insert.innerHTML += playlist(resultsTwo);
-          // insert.innerHTML += playlist(resultsThree);
+        let resultsTwo = serverRes.playlist.b;
+        let resultsThree = serverRes.playlist.c;
+          insert.innerHTML = playlistRoute(resultsOne, 1);
+          insert.innerHTML += playlist(resultsTwo);
+          insert.innerHTML += playlist(resultsThree);
     });
   },
 
   radioTwo: function(){
     let radioOneCollection = new bbcRadioCollection('2');
       radioOneCollection.fetch().then(function(serverRes){
-        let results = serverRes.playlist.a;
-          insert.innerHTML = playlist(results);
+        let resultsOne = serverRes.playlist.a;
+        let resultsTwo = serverRes.playlist.b;
+        let resultsThree = serverRes.playlist.c;
+          insert.innerHTML = playlistRoute(resultsOne, 2);
+          insert.innerHTML += playlist(resultsTwo);
+          insert.innerHTML += playlist(resultsThree);
+      });
+  },
+
+  radioThree: function(){
+    let radioOneCollection = new bbcRadioCollection('2');
+      radioOneCollection.fetch().then(function(serverRes){
+        let results = serverRes.playlist.greatestriffs;
+          insert.innerHTML = playlistGreatest(results);
       });
   },
 
--- src/scripts/templates.js
+++ src/scripts/templates.js
@@ -24,3 +24,34 @@ export const playlist = function(obj){
   }).join('');
   return objContent;
 };
+
+export const playlistRoute = function(obj, route){
+  let finalStr = `<div class="second-nav">
+                  <div class="routes new"><a href="#radio/${route}/morning">Morning</a></div>
+                  <div class="routes new"><a href="#radio/${route}/afternoon">Afternoon</a></div>
+                  <div class="routes new"><a href="#radio/${route}/evening">Evening</a></div>
+                  </div>`;
+  let objContent = obj.map(function(listEl){
+    return `<div class="playlist">
+            <img class="border" src="${listEl.image}"/>
+            <div class="titles">
+            <h3>${listEl.artist}</h3>
+            <h6>'${listEl.title}'</h6>
+            </div>
+            </div>`
+  }).join('');
+  return finalStr + objContent;
+};
+
+export const playlistGreatest = function(obj){
+  let objContent = obj.map(function(listEl){
+    return `<div class="playlist">
+            <img class="border" src="${listEl.image}"/>
+            <div class="titles">
+            <h3>${listEl.artist}</h3>
+            <h6>'${listEl.title}'</h6>
+            </div>
+            </div>`
+  }).join('');
+  return objContent;
+};
